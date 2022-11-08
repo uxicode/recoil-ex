7754ae9eb6732096902ac16d6081e0ea63e1bcfd
bong cheol jeon <bongcheoljeon@bongui-MacBookPro.local>
2022-11-09 00:41:35 +0900

    first

---
+++ src/atom/textState.ts
@@ -0,0 +1,6 @@
+import {atom} from "recoil";
+
+const textState=atom({
+    key:'textState',
+    default:''
+})
---
+++ src/component/CharacterCounter.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterCounter = () => {
+    return (
+        <div>
+            <p>리코일 테스트</p>
+        </div>
+    );
+};
+
+export default CharacterCounter;
